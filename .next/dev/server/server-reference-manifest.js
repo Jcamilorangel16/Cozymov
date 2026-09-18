e8513386f6a366b4bc85e87a14354b7f5ca2dfc8
avillacis-art <avillacis@unicesar.edu.co>
2026-09-18 11:31:24 -0500

    Feact: Actualizacion de librerias en el Next.js y React parea el control de versiones

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U2CkYAFeQMDsmoKFJESbR+nfUkNykm4832oXvYMkP2Y=\"\n}"
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Hola Ash, Next.js está funcionando!</h1>;
+}
