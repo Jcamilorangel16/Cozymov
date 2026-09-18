e2aff1f4071ef4e4e72f2802c8158b37f7a295dd
Yeferson Pazo <yepazo@unicesar.edu.co>
2026-09-18 11:31:22 -0500

    Feact: Actualizacion de libreriass en el Next.js y React para control de versiones

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"53KL6mWvP+h2jHQAjoB+mLn8mC3tjw7f4rfcQpchhXY=\"\n}"
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 export default function Home() {
+<<<<<<< HEAD
   return <h1>Hola JuanDv 👋, Next.js está funcionando!</h1>;
 }
 
+=======
+  return <h1>Hola Yeferson 👋, Next.js está funcionando!</h1>;
+}
+>>>>>>> 9260eb5 (Feact: Actualizacion de libreriass en el Next.js y React para control de versiones)
