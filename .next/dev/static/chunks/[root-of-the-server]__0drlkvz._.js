bfe7c1672822f2c798e1bf5598114bd837a6514d
Yeferson Pazo <yepazo@unicesar.edu.co>
2026-09-22 17:23:29 -0500

    feact: add src in the frontend

--- .next/dev/static/chunks/[root-of-the-server]__0drlkvz._.js
+++ .next/dev/static/chunks/[root-of-the-server]__0drlkvz._.js
@@ -34,7 +34,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$react$2f$jsx
 ;
 function Home() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$client$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
-        children: "Hola Yeferson 👋, Next.js está funcionando!"
+        children: "Hola JuanDv 👋, Next.js está funcionando!"
     }, void 0, false, {
         fileName: "[project]/pages/index.js",
         lineNumber: 2,
