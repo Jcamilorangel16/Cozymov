a5a653a8e6f2bc652758c5fe2fc021de0c4ad552
Yeferson Pazo <yepazo@unicesar.edu.co>
2026-09-25 11:43:49 -0500

    Feat: Borre pages

--- pages/_app.js
+++
@@ -1,5 +0,0 @@
-import '../Frontend/src/features/register/pages/register.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
--- pages/register.js
+++
@@ -1,1 +0,0 @@
-export { default } from '../Frontend/src/features/register/pages/register.jsx';
